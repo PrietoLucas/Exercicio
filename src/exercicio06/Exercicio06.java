16b3d229c12569dfc7768c44d9ed8dff1abe98ba
Lucas Prieto de Senna <lucas-prieto@hotmail.com>
2020-02-13 14:39:17 -0300

    Add exercicio 06

---
+++ src/exercicio06/Exercicio06.java
@@ -0,0 +1,8 @@
+package exercicio06;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        System.out.println("Encapsulamento serve para isolar e ter um maior controle sobre atributos e metodos de uma classe!");
+        System.out.println("Polimorfismo significa varias formas!");
+    }
+}
